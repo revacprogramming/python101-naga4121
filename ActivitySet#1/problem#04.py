49909c5b4dbbcf15242e6f90c4cb716b2e752f20
Naga Koushik <94678196+naga4121@users.noreply.github.com>
2022-05-26 10:24:30 +0000

    done

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,11 @@
-# Conditional Execution
-
-hrs = input("Enter hours? ")
+hrs=float(input("input hours"))
+rate=float("enter hours")
+if hrs<=40:
+  pay=hrs*rate
+  print(pay)
+else:
+  ovrhrs=(hrs-40)
+  regular=rate*40
+  othrs=ovrhrs*(rate*1.5)
+  overpay=(regular+othrs)
+     print(ovrpay)
